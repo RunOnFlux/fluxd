8a0d4a675adc4ece1c2f03ffd6014435c1c4b27a
Tadeáš Kmenta <TheTrunk@users.noreply.github.com>
2018-07-09 13:18:52 +0200

    Heights, difficulty reset

--- src/pow.cpp
+++ src/pow.cpp
@@ -22,12 +22,18 @@
 
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
+    const CChainParams& chainParams = Params();
     unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
 
     // Genesis block
     if (pindexLast == NULL)
         return nProofOfWorkLimit;
 
+    // Reset the difficulty after the algo fork and keep it for 30 blocks. LWMA averaging window is 60 blocks.
+    if (pindexLast->nHeight > chainParams.eh_epoch_1_end() - 1
+        && pindexLast->nHeight < chainParams.eh_epoch_1_end() + 30) {
+        return nProofOfWorkLimit;
+    }
     // Find the first block in the averaging interval
     const CBlockIndex* pindexFirst = pindexLast;
     arith_uint256 bnTot {0};
