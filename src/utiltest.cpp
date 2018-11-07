755d5937bcf735e3d9dd88e2e8308c4f23202b3e
Eirik Ogilvie-Wigley <eirik@z.cash>
2018-11-07 13:44:57 -0700

    benchmarks do not require updating network parameters

--- src/utiltest.cpp
+++ src/utiltest.cpp
@@ -168,8 +168,6 @@ const Consensus::Params& ActivateAcadia() {
 void DeactivateAcadia() {
     UpdateNetworkUpgradeParameters(Consensus::UPGRADE_ACADIA, Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT);
     UpdateNetworkUpgradeParameters(Consensus::UPGRADE_ACADIA, Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT);
-    // Consider: Should we be doing the following?
-    // SelectParams(CBaseChainParams::MAIN);
 }
 
 libzelcash::SaplingExtendedSpendingKey GetMasterSaplingSpendingKey() {
