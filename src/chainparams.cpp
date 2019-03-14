29b4014e38494bb7a14fa57558926b75b261d63e
Sean Bowe <ewillbefull@gmail.com>
2019-03-14 15:39:11 -0600

    Check blockhash of fallback block for Sprout value pool balance

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -332,6 +332,7 @@ public:
         nSproutValuePoolCheckpointHeight = 440329;
         nSproutValuePoolCheckpointBalance = 40000029096803;
         fSproutValuePoolCheckpointEnabled = true;
+        hashSproutValuePoolCheckpointBlock = uint256S("000a95d08ba5dcbabe881fc6471d11807bcca7df5f1795c99f3ec4580db4279b");
 
     }
 };
@@ -444,7 +445,10 @@ public:
 
         // Enable Sprout shielded value pool checkpointing on
         // regtest.
+        nSproutValuePoolCheckpointHeight = 0;
+        nSproutValuePoolCheckpointBalance = 0;
         fSproutValuePoolCheckpointEnabled = true;
+        hashSproutValuePoolCheckpointBlock = consensus.hashGenesisBlock;
     }
 
     void UpdateNetworkUpgradeParameters(Consensus::UpgradeIndex idx, int nActivationHeight)
