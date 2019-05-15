861a153904aaa8e51816f6e56cc6b4872976eeff
milesmanley <hey@milesmanley.com>
2019-05-16 01:17:21 +0900

    Copyright year bump

--- src/rpc/zelnode.cpp
+++ src/rpc/zelnode.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2012 The Bitcoin developers
-// Copyright (c) 2015-2018 The PIVX developers
-// Copyright (c) 2018-2019 The Zelcash developers
+// Copyright (c) 2015-2019 The PIVX developers
+// Copyright (c) 2018-2019 The Zel developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
