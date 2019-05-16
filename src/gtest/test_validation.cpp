rkass/zcash

Rename methods to include Sprout

src/gtest/test_validation.cpp
@@ -91,14 +91,14 @@ TEST(Validation, ReceivedBlockTransactions) {
 
     // Create a fake genesis block
     CBlock block1;
-    block1.vtx.push_back(GetValidReceive(*params, sk, 5, true));
+    block1.vtx.push_back(GetValidSproutReceive(*params, sk, 5, true));
     block1.hashMerkleRoot = block1.BuildMerkleTree();
     CBlockIndex fakeIndex1 {block1};
 
     // Create a fake child block
     CBlock block2;
     block2.hashPrevBlock = block1.GetHash();
-    block2.vtx.push_back(GetValidReceive(*params, sk, 10, true));
+    block2.vtx.push_back(GetValidSproutReceive(*params, sk, 10, true));
     block2.hashMerkleRoot = block2.BuildMerkleTree();
     CBlockIndex fakeIndex2 {block2};
     fakeIndex2.pprev = &fakeIndex1;

src/utiltest.cpp
@@ -8,10 +8,11 @@
 
 #include <array>
 
-CWalletTx GetValidReceive(ZCJoinSplit& params,
-                          const libzcash::SproutSpendingKey& sk, CAmount value,
-                          bool randomInputs,
-                          int32_t version /* = 2 */) {
+CWalletTx GetValidSproutReceive(ZCJoinSplit& params,
+                                const libzcash::SproutSpendingKey& sk,
+                                CAmount value,
+                                bool randomInputs,
+                                int32_t version /* = 2 */) {
     CMutableTransaction mtx;
     mtx.nVersion = version;
     mtx.vin.resize(2);
@@ -70,9 +71,9 @@ CWalletTx GetValidReceive(ZCJoinSplit& params,
     return wtx;
 }
 
-libzcash::SproutNote GetNote(ZCJoinSplit& params,
-                       const libzcash::SproutSpendingKey& sk,
-                       const CTransaction& tx, size_t js, size_t n) {
+libzcash::SproutNote GetSproutNote(ZCJoinSplit& params,
+                                   const libzcash::SproutSpendingKey& sk,
+                                   const CTransaction& tx, size_t js, size_t n) {
     ZCNoteDecryption decryptor {sk.receiving_key()};
     auto hSig = tx.vjoinsplit[js].h_sig(params, tx.joinSplitPubKey);
     auto note_pt = libzcash::SproutNotePlaintext::decrypt(
@@ -84,9 +85,10 @@ libzcash::SproutNote GetNote(ZCJoinSplit& params,
     return note_pt.note(sk.address());
 }
 
-CWalletTx GetValidSpend(ZCJoinSplit& params,
-                        const libzcash::SproutSpendingKey& sk,
-                        const libzcash::SproutNote& note, CAmount value) {
+CWalletTx GetValidSproutSpend(ZCJoinSplit& params,
+                              const libzcash::SproutSpendingKey& sk,
+                              const libzcash::SproutNote& note,
+                              CAmount value) {
     CMutableTransaction mtx;
     mtx.vout.resize(2);
     mtx.vout[0].nValue = value;

src/utiltest.h
@@ -7,13 +7,15 @@
 #include "zcash/Note.hpp"
 #include "zcash/NoteEncryption.hpp"
 
-CWalletTx GetValidReceive(ZCJoinSplit& params,
-                          const libzcash::SproutSpendingKey& sk, CAmount value,
-                          bool randomInputs,
-                          int32_t version = 2);
-libzcash::SproutNote GetNote(ZCJoinSplit& params,
-                       const libzcash::SproutSpendingKey& sk,
-                       const CTransaction& tx, size_t js, size_t n);
-CWalletTx GetValidSpend(ZCJoinSplit& params,
-                        const libzcash::SproutSpendingKey& sk,
-                        const libzcash::SproutNote& note, CAmount value);
+CWalletTx GetValidSproutReceive(ZCJoinSplit& params,
+                                const libzcash::SproutSpendingKey& sk,
+                                CAmount value,
+                                bool randomInputs,
+                                int32_t version = 2);
+libzcash::SproutNote GetSproutNote(ZCJoinSplit& params,
+                                   const libzcash::SproutSpendingKey& sk,
+                                   const CTransaction& tx, size_t js, size_t n);
+CWalletTx GetValidSproutSpend(ZCJoinSplit& params,
+                              const libzcash::SproutSpendingKey& sk,
+                              const libzcash::SproutNote& note,
+                              CAmount value);

src/zcbenchmarks.cpp
@@ -289,7 +289,7 @@ double benchmark_try_decrypt_notes(size_t nAddrs)
     }
 
     auto sk = libzcash::SproutSpendingKey::random();
-    auto tx = GetValidReceive(*pzcashParams, sk, 10, true);
+    auto tx = GetValidSproutReceive(*pzcashParams, sk, 10, true);
 
     struct timeval tv_start;
     timer_start(tv_start);
@@ -309,8 +309,8 @@ double benchmark_increment_note_witnesses(size_t nTxs)
     // First block
     CBlock block1;
     for (int i = 0; i < nTxs; i++) {
-        auto wtx = GetValidReceive(*pzcashParams, sk, 10, true);
-        auto note = GetNote(*pzcashParams, sk, wtx, 0, 1);
+        auto wtx = GetValidSproutReceive(*pzcashParams, sk, 10, true);
+        auto note = GetSproutNote(*pzcashParams, sk, wtx, 0, 1);
         auto nullifier = note.nullifier(sk);
 
         mapSproutNoteData_t noteData;
@@ -332,8 +332,8 @@ double benchmark_increment_note_witnesses(size_t nTxs)
     CBlock block2;
     block2.hashPrevBlock = block1.GetHash();
     {
-        auto wtx = GetValidReceive(*pzcashParams, sk, 10, true);
-        auto note = GetNote(*pzcashParams, sk, wtx, 0, 1);
+        auto wtx = GetValidSproutReceive(*pzcashParams, sk, 10, true);
+        auto note = GetSproutNote(*pzcashParams, sk, wtx, 0, 1);
         auto nullifier = note.nullifier(sk);
 
         mapSproutNoteData_t noteData;

RunOnFlux/fluxd

Co-Authored-By: Eirik Ogilvie-Wigley <eirik0@users.noreply.github.com>

src/gtest/test_validation.cpp
@@ -91,14 +91,14 @@ TEST(Validation, ReceivedBlockTransactions) {
 
     // Create a fake genesis block
     CBlock block1;
-    block1.vtx.push_back(GetValidReceive(*params, sk, 5, true));
+    block1.vtx.push_back(GetValidSproutReceive(*params, sk, 5, true));
     block1.hashMerkleRoot = block1.BuildMerkleTree();
     CBlockIndex fakeIndex1 {block1};
 
     // Create a fake child block
     CBlock block2;
     block2.hashPrevBlock = block1.GetHash();
-    block2.vtx.push_back(GetValidReceive(*params, sk, 10, true));
+    block2.vtx.push_back(GetValidSporutReceive(*params, sk, 10, true));
     block2.hashMerkleRoot = block2.BuildMerkleTree();
     CBlockIndex fakeIndex2 {block2};
     fakeIndex2.pprev = &fakeIndex1;

src/utiltest.cpp
@@ -8,10 +8,11 @@
 
 #include <array>
 
-CWalletTx GetValidReceive(ZCJoinSplit& params,
-                          const libzelcash::SproutSpendingKey& sk, CAmount value,
-                          bool randomInputs,
-                          int32_t version /* = 2 */) {
+CWalletTx GetValidSproutReceive(ZCJoinSplit& params,
+                                const libzelcash::SproutSpendingKey& sk,
+                                CAmount value,
+                                bool randomInputs,
+                                int32_t version /* = 2 */) {
     CMutableTransaction mtx;
     mtx.nVersion = version;
     mtx.vin.resize(2);
@@ -70,9 +71,9 @@ CWalletTx GetValidReceive(ZCJoinSplit& params,
     return wtx;
 }
 
-libzelcash::SproutNote GetNote(ZCJoinSplit& params,
-                       const libzelcash::SproutSpendingKey& sk,
-                       const CTransaction& tx, size_t js, size_t n) {
+libzelcash::SproutNote GetSproutNote(ZCJoinSplit& params,
+                                   const libzelcash::SproutSpendingKey& sk,
+                                   const CTransaction& tx, size_t js, size_t n) {
     ZCNoteDecryption decryptor {sk.receiving_key()};
     auto hSig = tx.vjoinsplit[js].h_sig(params, tx.joinSplitPubKey);
     auto note_pt = libzelcash::SproutNotePlaintext::decrypt(
@@ -84,9 +85,10 @@ libzelcash::SproutNote GetNote(ZCJoinSplit& params,
     return note_pt.note(sk.address());
 }
 
-CWalletTx GetValidSpend(ZCJoinSplit& params,
-                        const libzelcash::SproutSpendingKey& sk,
-                        const libzelcash::SproutNote& note, CAmount value) {
+CWalletTx GetValidSproutSpend(ZCJoinSplit& params,
+                              const libzelcash::SproutSpendingKey& sk,
+                              const libzelcash::SproutNote& note,
+                              CAmount value) {
     CMutableTransaction mtx;
     mtx.vout.resize(2);
     mtx.vout[0].nValue = value;

src/utiltest.h
@@ -8,13 +8,15 @@
 #include "zelcash/Note.hpp"
 #include "zelcash/NoteEncryption.hpp"
 
-CWalletTx GetValidReceive(ZCJoinSplit& params,
-                          const libzelcash::SproutSpendingKey& sk, CAmount value,
-                          bool randomInputs,
-                          int32_t version = 2);
-libzelcash::SproutNote GetNote(ZCJoinSplit& params,
-                       const libzelcash::SproutSpendingKey& sk,
-                       const CTransaction& tx, size_t js, size_t n);
-CWalletTx GetValidSpend(ZCJoinSplit& params,
-                        const libzelcash::SproutSpendingKey& sk,
-                        const libzelcash::SproutNote& note, CAmount value);
+CWalletTx GetValidSproutReceive(ZCJoinSplit& params,
+                                const libzelcash::SproutSpendingKey& sk,
+                                CAmount value,
+                                bool randomInputs,
+                                int32_t version = 2);
+libzelcash::SproutNote GetSproutNote(ZCJoinSplit& params,
+                                   const libzelcash::SproutSpendingKey& sk,
+                                   const CTransaction& tx, size_t js, size_t n);
+CWalletTx GetValidSproutSpend(ZCJoinSplit& params,
+                              const libzelcash::SproutSpendingKey& sk,
+                              const libzelcash::SproutNote& note,
+                              CAmount value);

src/zcbenchmarks.cpp
@@ -289,7 +289,7 @@ double benchmark_try_decrypt_notes(size_t nAddrs)
     }
 
     auto sk = libzelcash::SproutSpendingKey::random();
-    auto tx = GetValidReceive(*pzelcashParams, sk, 10, true);
+    auto tx = GetValidSporutReceive(*pzelcashParams, sk, 10, true);
 
     struct timeval tv_start;
     timer_start(tv_start);
@@ -309,8 +309,8 @@ double benchmark_increment_note_witnesses(size_t nTxs)
     // First block
     CBlock block1;
     for (int i = 0; i < nTxs; i++) {
-        auto wtx = GetValidReceive(*pzelcashParams, sk, 10, true);
-        auto note = GetNote(*pzelcashParams, sk, wtx, 0, 1);
+        auto wtx = GetValidSproutReceive(*pzelcashParams, sk, 10, true);
+        auto note = GetSproutNote(*pzelcashParams, sk, wtx, 0, 1);
         auto nullifier = note.nullifier(sk);
 
         mapSproutNoteData_t noteData;
@@ -332,8 +332,8 @@ double benchmark_increment_note_witnesses(size_t nTxs)
     CBlock block2;
     block2.hashPrevBlock = block1.GetHash();
     {
-        auto wtx = GetValidReceive(*pzelcashParams, sk, 10, true);
-        auto note = GetNote(*pzelcashParams, sk, wtx, 0, 1);
+        auto wtx = GetValidSproutReceive(*pzelcashParams, sk, 10, true);
+        auto note = GetSproutNote(*pzelcashParams, sk, wtx, 0, 1);
         auto nullifier = note.nullifier(sk);
 
         mapSproutNoteData_t noteData;