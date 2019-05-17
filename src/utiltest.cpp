zcash/zcash

Add benchmark for decrypting sapling notes

src/utiltest.cpp
@@ -5,6 +5,7 @@
 #include "utiltest.h"
 
 #include "consensus/upgrades.h"
+#include "transaction_builder.h"
 
 #include <array>
 
@@ -150,6 +151,31 @@ CWalletTx GetValidSproutSpend(ZCJoinSplit& params,
                                 joinSplitPrivKey
                                ) == 0);
     CTransaction tx {mtx};
+    CWalletTx wtx {NULL, tx};
+    return wtx;
+}
+
+CWalletTx GetValidSaplingTx(const Consensus::Params& consensusParams,
+                            const libzcash::SaplingExtendedSpendingKey &sk,
+                            CAmount value) {
+    auto expsk = sk.expsk;
+    auto fvk = expsk.full_viewing_key();
+    auto pk = sk.DefaultAddress();
+
+    // Generate dummy Sapling note
+    libzcash::SaplingNote note(pk, value);
+    auto cm = note.cm().get();
+    SaplingMerkleTree tree;
+    tree.append(cm);
+    auto anchor = tree.root();
+    auto witness = tree.witness();
+
+    auto builder = TransactionBuilder(consensusParams, 1);
+    builder.SetFee(0);
+    assert(builder.AddSaplingSpend(expsk, note, anchor, witness));
+    builder.AddSaplingOutput(fvk.ovk, pk, value, {});
+
+    CTransaction tx = builder.Build().get();
     CWalletTx wtx {NULL, tx};
     return wtx;
 }

src/utiltest.h
@@ -6,7 +6,9 @@
 #include "zcash/JoinSplit.hpp"
 #include "zcash/Note.hpp"
 #include "zcash/NoteEncryption.hpp"
+#include "zcash/zip32.h"
 
+// Sprout
 CWalletTx GetValidSproutReceive(ZCJoinSplit& params,
                                 const libzcash::SproutSpendingKey& sk,
                                 CAmount value,
@@ -19,3 +21,8 @@ CWalletTx GetValidSproutSpend(ZCJoinSplit& params,
                               const libzcash::SproutSpendingKey& sk,
                               const libzcash::SproutNote& note,
                               CAmount value);
+
+// Sapling
+CWalletTx GetValidSaplingTx(const Consensus::Params& consensusParams,
+                            const libzcash::SaplingExtendedSpendingKey &sk,
+                            CAmount value);

src/zcbenchmarks.cpp
@@ -280,7 +280,7 @@ double benchmark_large_tx(size_t nInputs)
     return timer_stop(tv_start);
 }
 
-double benchmark_try_decrypt_notes(size_t nAddrs)
+double benchmark_try_decrypt_sprout_notes(size_t nAddrs)
 {
     CWallet wallet;
     for (int i = 0; i < nAddrs; i++) {
@@ -295,6 +295,42 @@ double benchmark_try_decrypt_notes(size_t nAddrs)
     timer_start(tv_start);
     auto nd = wallet.FindMySproutNotes(tx);
     return timer_stop(tv_start);
+}
+
+double benchmark_try_decrypt_sapling_notes(size_t nAddrs)
+{
+    // Set params
+    SelectParams(CBaseChainParams::REGTEST);
+    UpdateNetworkUpgradeParameters(Consensus::UPGRADE_OVERWINTER, Consensus::NetworkUpgrade::ALWAYS_ACTIVE);
+    UpdateNetworkUpgradeParameters(Consensus::UPGRADE_SAPLING, Consensus::NetworkUpgrade::ALWAYS_ACTIVE);
+    auto consensusParams = Params().GetConsensus();
+
+    std::vector<unsigned char, secure_allocator<unsigned char>> rawSeed(32);
+    HDSeed seed(rawSeed);
+    auto masterKey = libzcash::SaplingExtendedSpendingKey::Master(seed);
+
+    CWallet wallet;
+    wallet.AddSaplingSpendingKey(masterKey, masterKey.DefaultAddress());
+
+    int i;
+    for (i = 0; i < nAddrs; i++) {
+        auto sk = masterKey.Derive(i);
+        wallet.AddSaplingSpendingKey(sk, sk.DefaultAddress());
+    }
+
+    auto sk = masterKey.Derive(i);
+    auto tx = GetValidSaplingTx(consensusParams, sk, 10);
+
+    struct timeval tv_start;
+    timer_start(tv_start);
+    auto saplingNoteDataAndAddressesToAdd = wallet.FindMySaplingNotes(tx);
+    double tv_stop = timer_stop(tv_start);
+
+    // Revert to default
+    UpdateNetworkUpgradeParameters(Consensus::UPGRADE_SAPLING, Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT);
+    UpdateNetworkUpgradeParameters(Consensus::UPGRADE_OVERWINTER, Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT);
+
+    return tv_stop;
 }
 
 double benchmark_increment_note_witnesses(size_t nTxs)

RunOnFlux/fluxd

Co-Authored-By: Eirik Ogilvie-Wigley <eirik0@users.noreply.github.com>

src/utiltest.cpp
@@ -5,6 +5,8 @@
 #include "utiltest.h"
 
 #include "consensus/upgrades.h"
+#include "transaction_builder.h"
+
 
 #include <array>
 
@@ -150,6 +152,31 @@ CWalletTx GetValidSproutSpend(ZCJoinSplit& params,
                                 joinSplitPrivKey
                                ) == 0);
     CTransaction tx {mtx};
+    CWalletTx wtx {NULL, tx};
+    return wtx;
+}
+
+CWalletTx GetValidSaplingTx(const Consensus::Params& consensusParams,
+                            const libzelcash::SaplingExtendedSpendingKey &sk,
+                            CAmount value) {
+    auto expsk = sk.expsk;
+    auto fvk = expsk.full_viewing_key();
+    auto pk = sk.DefaultAddress();
+
+    // Generate dummy Sapling note
+    libzelcash::SaplingNote note(pk, value);
+    auto cm = note.cm().get();
+    SaplingMerkleTree tree;
+    tree.append(cm);
+    auto anchor = tree.root();
+    auto witness = tree.witness();
+
+    auto builder = TransactionBuilder(consensusParams, 1);
+    builder.SetFee(0);
+    assert(builder.AddSaplingSpend(expsk, note, anchor, witness));
+    builder.AddSaplingOutput(fvk.ovk, pk, value, {});
+
+    CTransaction tx = builder.Build().get();
     CWalletTx wtx {NULL, tx};
     return wtx;
 }

src/utiltest.h
@@ -7,7 +7,9 @@
 #include "zelcash/JoinSplit.hpp"
 #include "zelcash/Note.hpp"
 #include "zelcash/NoteEncryption.hpp"
+#include "zelcash/zip32.h"
 
+// Sprout
 CWalletTx GetValidSproutReceive(ZCJoinSplit& params,
                                 const libzelcash::SproutSpendingKey& sk,
                                 CAmount value,
@@ -20,3 +22,8 @@ CWalletTx GetValidSproutSpend(ZCJoinSplit& params,
                               const libzelcash::SproutSpendingKey& sk,
                               const libzelcash::SproutNote& note,
                               CAmount value);
+
+// Sapling
+CWalletTx GetValidSaplingTx(const Conesnsus::Params& consensusParams,
+                            const libzelcash::SaplingExtendedSpendingKey &sk,
+                            CAmount value);

src/zcbenchmarks.cpp
@@ -280,7 +280,7 @@ double benchmark_large_tx(size_t nInputs)
     return timer_stop(tv_start);
 }
 
-double benchmark_try_decrypt_notes(size_t nAddrs)
+double benchmark_try_decrypt_sprout_notes(size_t nAddrs)
 {
     CWallet wallet;
     for (int i = 0; i < nAddrs; i++) {
@@ -295,6 +295,40 @@ double benchmark_try_decrypt_notes(size_t nAddrs)
     timer_start(tv_start);
     auto nd = wallet.FindMySproutNotes(tx);
     return timer_stop(tv_start);
+}
+
+double benchmark_try_decrypt_sapling_notes(size_t nAddrs)
+{
+    // Set params
+    SelectParams(CBaseChainParams::REGTEST);
+    UpdateNetworkUpgradeParameters(Consensus::UPGRADE_ACADIA, Consensus::NetworkUpgrade::ALWAYS_ACTIVE);
+    auto consensusParams = Params().GetConsensus();
+
+    std::vector<unsigned char, secure_allocator<unsigned char>> rawSeed(32);
+    HDSeed seed(rawSeed);
+    auto masterKey = libzcash::SaplingExtendedSpendingKey::Master(seed);
+
+    CWallet wallet;
+    wallet.AddSaplingSpendingKey(masterKey, masterKey.DefaultAddress());
+
+    int i;
+    for (i = 0; i < nAddrs; i++) {
+        auto sk = masterKey.Derive(i);
+        wallet.AddSaplingSpendingKey(sk, sk.DefaultAddress());
+    }
+
+    auto sk = masterKey.Derive(i);
+    auto tx = GetValidSaplingTx(consensusParams, sk, 10);
+
+    struct timeval tv_start;
+    timer_start(tv_start);
+    auto saplingNoteDataAndAddressesToAdd = wallet.FindMySaplingNotes(tx);
+    double tv_stop = timer_stop(tv_start);
+
+    // Revert to default
+    UpdateNetworkUpgradeParameters(Consensus::UPGRADE_ACADIA, Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT);
+
+    return tv_stop;
 }
 
 double benchmark_increment_note_witnesses(size_t nTxs)