zcash/zcash

Add test method for generating master Sapling extended spending keys

src/utiltest.cpp
@@ -171,6 +171,12 @@ void DeactivateSapling() {
     // SelectParams(CBaseChainParams::MAIN);
 }
 
+libzcash::SaplingExtendedSpendingKey GetMasterSaplingSpendingKey() {
+    std::vector<unsigned char, secure_allocator<unsigned char>> rawSeed(32);
+    HDSeed seed(rawSeed);
+    return libzcash::SaplingExtendedSpendingKey::Master(seed);
+}
+
 TestSaplingNote GetTestSaplingNote(const libzcash::SaplingPaymentAddress& pa, CAmount value) {
     // Generate dummy Sapling note
     libzcash::SaplingNote note(pa, value);
@@ -188,7 +194,7 @@ CWalletTx GetValidSaplingTx(const Consensus::Params& consensusParams,
     auto pa = sk.DefaultAddress();
 
     auto testNote = GetTestSaplingNote(pa, value);
-
+ 
     auto builder = TransactionBuilder(consensusParams, 1);
     builder.SetFee(0);
     builder.AddSaplingSpend(expsk, testNote.note, testNote.tree.root(), testNote.tree.witness());

src/utiltest.h
@@ -32,6 +32,8 @@ const Consensus::Params& ActivateSapling();
 
 void DeactivateSapling();
 
+libzcash::SaplingExtendedSpendingKey GetMasterSaplingSpendingKey();
+
 /**
  * Generate a dummy SaplingNote and a SaplingMerkleTree with that note's commitment.
  */

RunOnFlux/fluxd

src/utiltest.cpp
@@ -172,6 +172,12 @@ void DeactivateAcadia() {
     // SelectParams(CBaseChainParams::MAIN);
 }
 
+libzelcash::SaplingExtendedSpendingKey GetMasterSaplingSpendingKey() {
+    std::vector<unsigned char, secure_allocator<unsigned char>> rawSeed(32);
+    HDSeed seed(rawSeed);
+    return libzelcash::SaplingExtendedSpendingKey::Master(seed);
+}
+
 TestSaplingNote GetTestSaplingNote(const libzelcash::SaplingPaymentAddress& pa, CAmount value) {
     // Generate dummy Sapling note
     libzelcash::SaplingNote note(pa, value);
@@ -189,7 +195,7 @@ CWalletTx GetValidSaplingTx(const Consensus::Params& consensusParams,
     auto pa = sk.DefaultAddress();
 
     auto testNote = GetTestSaplingNote(pa, value);
-
+ 
     auto builder = TransactionBuilder(consensusParams, 1);
     builder.SetFee(0);
     builder.AddSaplingSpend(expsk, testNote.note, testNote.tree.root(), testNote.tree.witness());

src/utiltest.h
@@ -33,6 +33,8 @@ const Consensus::Params& ActivateSapling();
 
 void DeactivateAcadia();
 
+libzelcash::SaplingExtendedSpendingKey GetMasterSaplingSpendingKey();
+
 /**
  * Generate a dummy SaplingNote and a SaplingMerkleTree with that note's commitment.
  */