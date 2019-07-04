530bef135042a1c124abad754de94b24995b92cf
Beshoy Girgis <shoy@1ds.us>
2019-07-03 23:09:48 -0500

    Update zeromq checksum

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -2,7 +2,7 @@ package=zeromq
 $(package)_version=4.3.1
 $(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=bcbabe1e2c7d0eec4ed612e10b94b112dd5f06fcefa994a0c79a45d835cd21eb
+$(package)_sha256_hash=acb8d8123f22451f43287e4e053ff52e9bce5aa24b748b314ca43d8114e9b939
 
 define $(package)_set_vars
   $(package)_config_opts=--without-documentation --disable-shared --disable-curve
