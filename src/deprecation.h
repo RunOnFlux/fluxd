f354cc1adcb36912454a3055be8bcac667a5fa1c
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2020-07-13 19:00:01 +0200

    update deprecation policy

--- src/deprecation.h
+++ src/deprecation.h
@@ -7,11 +7,11 @@
 #define ZELCASH_DEPRECATION_H
 
 // Deprecation policy:
-// * Shut down 16 weeks' worth of blocks after the estimated release block height.
+// * Shut down 52 weeks' worth of blocks after the estimated release block height.
 // * A warning is shown during the 2 weeks' worth of blocks prior to shut down.
 
-static const int APPROX_RELEASE_HEIGHT = 820000; // 1 year after Kamata activation
-static const int WEEKS_UNTIL_DEPRECATION = 26;
+static const int APPROX_RELEASE_HEIGHT = 642000;
+static const int WEEKS_UNTIL_DEPRECATION = 52;
 static const int DEPRECATION_HEIGHT = APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 24 * 30);
 
 // Number of blocks before deprecation to warn users
