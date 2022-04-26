6d2e1fdacf3a0886e5d8a271651fdcf8ec73a6c9
Tom Moulton <tom@rnode.moulton.us>
2022-04-26 08:41:00 -0400

    Update generated Copyright message

--- src/clientversion.h
+++ src/clientversion.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2014 The Bitcoin Core developers
 // Copyright (c) 2016-2017 The Zcash developers
+// Copyright (C) 2018-2022 The Flux Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
@@ -27,7 +28,7 @@
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2019
+#define COPYRIGHT_YEAR 2022
 
 #endif //HAVE_CONFIG_H
 
@@ -39,8 +40,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, The Zcash developers and The Zel developers"
-// Copyright (C) 2018-2022 The Flux Developers
+#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, The Zcash developers, The Zel developers and The Flux Developers"
 
 /**
  * bitcoind-res.rc includes this file, but it cannot cope with real c++ code.
