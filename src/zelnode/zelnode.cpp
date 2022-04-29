c3641af83e6a4473a98f22de587abc53c38a8620
Tom Moulton <tom@rnode.moulton.us>
2022-04-29 07:20:11 -0400

    Added runtime error if not fFluxnode for good measure, other RPC calls use it too

--- src/zelnode/zelnode.cpp
+++ src/zelnode/zelnode.cpp
@@ -1137,11 +1137,6 @@ void FluxnodeCache::DumpFluxnodeCache()
     }
 }
 
-bool IsDFluxnodeActive()
-{
-    return chainActive.Height() >= Params().StartFluxnodePayments();
-}
-
 bool IsFluxnodeTransactionsActive()
 {
     return chainActive.Height() >= Params().GetConsensus().vUpgrades[Consensus::UPGRADE_KAMATA].nActivationHeight;
