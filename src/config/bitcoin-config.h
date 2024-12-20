827ea75c968450c00a6c077b585709b7e386d60d
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2024-12-20 15:57:21 +0800

    make-release.py: Updated manpages for 7.2.0.

--- src/config/bitcoin-config.h
+++ src/config/bitcoin-config.h
@@ -18,7 +18,7 @@
 #define CLIENT_VERSION_MAJOR 7
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 1
+#define CLIENT_VERSION_MINOR 2
 
 /* Build revision */
 #define CLIENT_VERSION_REVISION 0
@@ -290,7 +290,7 @@
 #define PACKAGE_NAME "Flux"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Flux 7.1.0"
+#define PACKAGE_STRING "Flux 7.2.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "flux"
@@ -299,7 +299,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "7.1.0"
+#define PACKAGE_VERSION "7.2.0"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
