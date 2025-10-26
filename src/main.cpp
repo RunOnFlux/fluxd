f226a65c35706532a368a091c72de14a9bcf2a53
TheTrunk <tadeas@runonflux.io>
2025-10-26 20:45:32 +0800

    add checkpoints, reorg limit increase

--- src/main.cpp
+++ src/main.cpp
@@ -8048,8 +8048,9 @@ we need to compare similar types or i < j will not function correctly
 You must compare (int < int) because (int < unsigned int) can give incorrectly results
 */
 int64_t GetMaxReorgDepth(const int64_t nHeight) {
-    if (nHeight > MAX_REORG_LENGTH_UPDATED_HEIGHT) {
-        return MAX_REORG_LENGTH_UPDATED;
+    if (nHeight >= 2020000 && nHeight <= 2022500) {
+        return 2500;  // Temporary deep reorg limit during PON stabilization period
     }
-    return MAX_REORG_LENGTH;
+
+    return MAX_REORG_LENGTH;  // Normal limit: 40 blocks
 }
--- src/main.h
+++ src/main.h
@@ -71,9 +71,7 @@ static const bool DEFAULT_ALERTS = true;
 /** Minimum alert priority for enabling safe mode. */
 static const int ALERT_PRIORITY_SAFE_MODE = 4000;
 /** Maximum reorg length we will accept. */
-static const unsigned int MAX_REORG_LENGTH = 29;
-static const unsigned int MAX_REORG_LENGTH_UPDATED = 9;
-static const int64_t MAX_REORG_LENGTH_UPDATED_HEIGHT = 1350000;
+static const unsigned int MAX_REORG_LENGTH = 40;
 /** Maximum number of signature check operations in an IsStandard() P2SH script */
 static const unsigned int MAX_P2SH_SIGOPS = 15;
 /** The maximum number of sigops we're willing to relay/mine in a single tx */
