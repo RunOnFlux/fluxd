faa8398396909df52f7f29f57d011a7775d7b0ed
Tadeas Kmenta <tadeas@runonflux.io>
2026-05-19 03:07:55 +0000

    make-release.py: Updated manpages for 9.1.0.

--- src/config/bitcoin-config.h
+++ src/config/bitcoin-config.h
@@ -18,10 +18,10 @@
 #define CLIENT_VERSION_MAJOR 9
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 0
+#define CLIENT_VERSION_MINOR 1
 
 /* Build revision */
-#define CLIENT_VERSION_REVISION 6
+#define CLIENT_VERSION_REVISION 0
 
 /* Version is release */
 #define COPYRIGHT_YEAR 2022
@@ -269,7 +269,7 @@
 #define PACKAGE_NAME "Flux"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Flux 9.0.6"
+#define PACKAGE_STRING "Flux 9.1.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "flux"
@@ -278,7 +278,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "9.0.6"
+#define PACKAGE_VERSION "9.1.0"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
